d48763a1f44904d7394e4045575da8bc11d01fea
Janmejay Singh <singh.janmejay@gmail.com>
2024-10-29 12:43:22 +0530

    Add header documentation

--- include/sysfail.h
+++ include/sysfail.h
@@ -7,76 +7,177 @@
 
 typedef pid_t sysfail_tid_t;
 
+/**
+ * Probability values are in the range [0, 1].
+ * 0 means never fail or delay, 1 means always fail or delay.
+ */
 struct {
+    // Probability of failure / delay
     double p;
+
+    // Bias of failure, lower values make failure more likely before syscall
+    // and higher values make failure more likely after syscall.
+    // This is especially useful for testing idempotency properties of the
+    // application.
     double after_bias;
 } typedef sysfail_probability_t;
 
+/**
+ * Error weights are used to determine the mix of errors presented when a
+ * syscall is failed.
+ */
 struct {
-    int nerror; // errno
+    // Error number (errno) to be presented to the application.
+    int nerror;
+    // Relative weight, higher weight makes the error more likely. This does not
+    // affect the probability of failure, only the distribution of errors.
     double weight;
 } typedef sysfail_error_wt_t;
 
+/**
+ * `sysfail_userdata_t` is the user data that is passed to callbacks
+ * (predicates etc). Sysfail does not try to interpret this data.
+ */
 typedef void sysfail_userdata_t;
 
+/**
+ * `sysfail_syscall` is the syscall number.
+ */
 int sysfail_syscall(const greg_t*);
+
+/**
+ * `sysfail_syscall_arg` is the argument number.
+ */
 greg_t sysfail_syscall_arg(const greg_t*, int);
 
+/**
+ * `sysfail_invocation_predicate_t` is the predicate that determines if the
+ * syscall is eligible for failure injection.
+ */
 typedef int(*sysfail_invocation_predicate_t)(sysfail_userdata_t*, const greg_t*);
 
+/**
+ * `sysfail_outcome_t` is the outcome of a syscall.
+ */
 struct {
+    // Probability and bias of failure
     sysfail_probability_t fail;
+    // Probability and bias of delay
     sysfail_probability_t delay;
+    // Maximum delay in microseconds
     uint32_t max_delay_usec;
 
+    // User data
     sysfail_userdata_t* ctx;
+    // Eligibility predicate
     sysfail_invocation_predicate_t eligible;
 
+    // Number of errors to form the mix to be drwan from when the syscall fails
     uint32_t num_errors;
+    // Actual error codes and their weights
     sysfail_error_wt_t error_wts[];
 } typedef sysfail_outcome_t;
 
+/**
+ * `sysfail_thread_predicate_t` is the predicate that determines if the thread
+ * is eligible for failure injection.
+ */
 typedef int(*sysfail_thread_predicate_t)(sysfail_userdata_t*, sysfail_tid_t);
 
+/**
+ * `sysfail_thread_discovery_strategy_t` is the strategy to discover threads.
+ * Test can choose to have the threads automatically discovered or manually
+ * add / remove threads that are failure injected.
+ * Alternatively, the test can choose to poll the threads using the session API
+ * to discover threads at specific points in the execution of test.
+ */
 enum {
+    // No thread discovery strategy, add / remove / discover threads manually
     sysfail_tdisc_none  = 0,
+    // Poll to discover threads at regular intervals, manual controls can also
+    // be used in conjunction with automatic discovery.
     sysfail_tdisk_poll  = 1,
 } typedef sysfail_thread_discovery_strategy_t;
 
+/**
+ * `sysfail_thread_discovery_t` is the configuration for thread discovery.
+ */
 union {
+    // Polling interval in microseconds
     uint32_t poll_itvl_usec;
 } typedef sysfail_thread_discovery_t;
 
+/**
+ * `sysfail_syscall_outcome_t` is the outcome of a syscall.
+ */
 typedef struct sysfail_syscall_outcome_s sysfail_syscall_outcome_t;
-
 struct sysfail_syscall_outcome_s {
+    // Next outcome in the chain (linked list)
     sysfail_syscall_outcome_t* next;
+
+    // Syscall number
     int syscall;
+
+    // Outcome of the syscall
     sysfail_outcome_t outcome;
 };
 
+/**
+ * `sysfail_plan_t` is the overall plan for failure injection.
+ */
 struct {
+    // Strategy for thread discovery
     sysfail_thread_discovery_strategy_t strategy;
+    // Configuration for thread discovery
     sysfail_thread_discovery_t config;
 
+    // User data
     sysfail_userdata_t *ctx;
+    // Predicate to determine if the thread is eligible for failure injection
     sysfail_thread_predicate_t selector;
 
+    // Outcomes for syscalls (list)
     sysfail_syscall_outcome_t* syscall_outcomes;
 } typedef sysfail_plan_t;
 
+/**
+ * `sysfail_session_t` is the session for failure injection.
+ */
 typedef struct sysfail_session_s sysfail_session_t;
-
 struct sysfail_session_s {
+    // (Internal) Sysfail data
     void* data;
+
+    // Stop the session and free all resources (including the session itself)
+    // Caller should not use the session after stop.
+    // Plan is not freed, caller must free the plan independently.
     void (*stop)(sysfail_session_t*);
+
+    // Enable failure injection on the current (calling) thread
     void (*add_this_thread)(sysfail_session_t*);
+    // Disable failure injection on the current (calling) thread
     void (*remove_this_thread)(sysfail_session_t*);
+
+    // Enable failure injection on the thread with the given tid
     void (*add_thread)(sysfail_session_t*, sysfail_tid_t);
+
+    // Disable failure injection on the thread with the given tid
     void (*remove_thread)(sysfail_session_t*, sysfail_tid_t);
+
+    // Discover threads using the strategy configured in the plan
     void (*discover_threads)(sysfail_session_t*);
 };
 
+/**
+ * Start failure injection in the process. Starts the thread-discovery strategy.
+ * Returns the session that can be used to control failure injection.
+ * Caller must free the session using the `stop` method (not directly).
+ * Caller must free the plan independently.
+ *
+ * Depending on discovery strategy, all or just the calling thread are
+ * presented to the thread selector. Any threads chosen by the selector are
+ * failure injected.
+ */
 sysfail_session_t* sysfail_start(const sysfail_plan_t*);
 
 #endif
--- include/sysfail.hh
+++ include/sysfail.hh
@@ -13,13 +13,22 @@
 #include <stdexcept>
 
 namespace sysfail {
+    // Syscall number
     using Syscall = int;
+    // Error number (errno)
     using Errno = int;
+    // Signal number
     using Signal = int;
 
+    /**
+     * Probability of failure / delay
+     */
     struct Probability {
-        const double p;          // [0, 1] 0 => never fail, 1 => always fail
-        const double after_bias; // [0, 1] 0 => before syscall, 1 => after syscall
+        // [0, 1] 0 => never fail, 1 => always fail
+        const double p;
+         // [0, 1] 0 => before syscall, 1 => after syscall
+        const double after_bias;
+
         Probability(
             double p,
             double after_bias = 0
@@ -33,54 +42,89 @@ namespace sysfail {
         }
     };
 
+    // Invocation predicate filters syscall invocations at per-invocation level.
+    // Call is failure-injected only if the predicate returns true.
+    // This or one of the arity-specific types (below) can be used to filter
+    // syscalls.
     using InvocationPredicate = const std::function<bool(const greg_t*)>;
 
     namespace invp {
+        // Syscall argument
         using A = greg_t;
+        // Predicate for syscalls that do not accept any arguments.
+        // Eg. SYS_getpid
         using Zero = std::function<bool(Syscall)>;
+        // Predicate for syscalls that accept one argument. Eg. SYS_exit_group
         using One = std::function<bool(Syscall, A)>;
+        // Predicate for syscalls that accept two arguments. Eg. SYS_listen
         using Two = std::function<bool(Syscall, A, A)>;
+        // Predicate for syscalls that accept three arguments. Eg. SYS_write
         using Three = std::function<bool(Syscall, A, A, A)>;
+        // Predicate for syscalls that accept four arguments. Eg. SYS_openat2
         using Four = std::function<bool(Syscall, A, A, A, A)>;
+        // Predicate for syscalls that accept five arguments. Eg. SYS_pwritev
         using Five = std::function<bool(Syscall, A, A, A, A, A)>;
+        // Predicate for syscalls that accept all six arguments. Eg. SYS_mmap
         using Six = std::function<bool(Syscall, A, A, A, A, A, A)>;
 
+        // Arity independent predicate type
         using P = std::variant<Zero, One, Two, Three, Four, Five, Six>;
 
+        // Generates a general invocation predicate given arity-aware definition
         InvocationPredicate p(P p);
     }
 
+    /**
+     * Outcome of a syscall
+     */
     struct Outcome {
+        // Probability of failure
         const Probability fail;
+        // Probability of delay
         const Probability delay;
+        // Maximum delay in microseconds
         const std::chrono::microseconds max_delay;
+        // Errors to be presented to the call=site when failure is injected
+        // and relative weights. Higher weight makes the error more likely. This
+        // does not affect the probability of failure, only the distribution of
+        // errors when the syscall fails.
         const std::map<Errno, double> error_weights;
+        // Eligibility predicate for the syscall
         InvocationPredicate eligible;
     };
-
-    struct AddrRange;
 
     namespace thread_discovery {
         using namespace std::chrono_literals;
 
-        // Poll /proc/<pid>/task every itvl
+        // Either add / remove threads manually or use Session API to trigger a
+        // single isolated poll to discover threads. Sysfail does not
+        // auto-detect and inject failures into new threads.
+        struct None {};
+
+        // Poll at regular intervals to discover new threads. Manual controls
+        // such as add / remove / discover can also be used in conjunction with
+        // automatic discovery.
         struct ProcPoll {
             const std::chrono::microseconds itvl;
 
             ProcPoll( std::chrono::microseconds itvl = 10ms) : itvl(itvl) {}
         };
 
-        // Either add / remove threads manually or use Session API to trigger a
-        // single isolated poll of /proc/<pid>/task
-        struct None {};
-
+        // Strategy for thread discovery
         using Strategy = std::variant<ProcPoll, None>;
     }
 
+    /**
+     * Plan for failure injection
+     */
     struct Plan {
+        // Outcome by syscall
         const std::unordered_map<Syscall, const Outcome> outcomes;
+        // Predicate that picks threads eligible for failure injection
         const std::function<bool(pid_t)> selector;
+        // Strategy for thread discovery
         const thread_discovery::Strategy thd_disc;
+
         Plan(
             const std::unordered_map<Syscall, const Outcome>& outcomes,
             const std::function<bool(pid_t)>& selector,
@@ -96,15 +140,35 @@ namespace sysfail {
             thd_disc(thread_discovery::None{}) {}
     };
 
+    /**
+     * Session is the top-level handle for failure-injection in the process.
+     *
+     * Plan controls the failure and delay injection behavior while APIs on the
+     * session allow test / application to control behavior at thread or
+     * process level.
+     */
     class Session {
         std::shared_mutex lck;
     public:
-        explicit Session(const Plan& _plan);
+        // Start failure / delay injection. Based on the thread-discovery
+        // strategy in the plan failure injection may be enabled across all
+        // or just the calling thread. All threads regardless of the strategy
+        // or manual controls are first presented to the selector predicate and
+        // failure-injected only if the predicate returns true.
+        explicit Session(const Plan& plan);
+        // Stop failure / delay injection and terminate the session.
         ~Session();
+        // Enable failure / delay injection for the calling thread.
         void add();
+        // Disable failure / delay injection for the calling thread.
         void remove();
+        // Enable failure / delay injection for the thread with the given tid.
         void add(pid_t tid);
+        // Disable failure / delay injection for the thread with the given tid.
         void remove(pid_t tid);
+        // Discover threads on-demand. This can be used by the test /
+        // application to trigger a single isolated poll to discover threads and
+        // can be used regardless of the thread-discovery strategy in the plan.
         void discover_threads();
     };
 }
